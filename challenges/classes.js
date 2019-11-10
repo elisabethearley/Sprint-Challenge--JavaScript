6b845eaedbf8762f0142e97a42d2d845c1a95b0f
elisabethearley <elisabethearley@gmail.com>
2019-11-09 23:50:11 -0500

    completed Stretch on classes.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -2,26 +2,55 @@
 
 class CuboidMaker {
     constructor(props) {
-      this.length = props.length;
-      this.width = props.width;
-      this.height = props.height;
+        this.length = props.length;
+        this.width = props.width;
+        this.height = props.height;
     }
     volume() {
-      return this.length * this.width * this.height;
+        return this.length * this.width * this.height;
     }
     surfaceArea() {
-      return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
     }
   }
+
+class CubeMaker extends CuboidMaker {
+    constructor(props) {
+        super(props); 
+    }
+
+    cubeVolume() {
+        return this.length ** 3;
+    }
+    cubeSurfaceArea() {
+        return 6 * (this.length ** 2);
+    }
+}
   
-  const cuboid = new CuboidMaker ({
+const cuboid = new CuboidMaker ({
     length: 4,
     width: 5,
     height: 5
-  });
+});
 
-// Test your volume and surfaceArea methods by uncommenting the logs below:
+const newCube = new CubeMaker ({
+    length: 4,
+    width: 5,
+    height: 5
+})
+
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
+console.log(newCube.cubeVolume());
+console.log(newCube.cubeSurfaceArea());
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+// Test your volume and surfaceArea methods by uncommenting the logs below:
+
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  
+//Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  
+//Test your work by logging out your volume and surface area.
+
+
+
+ 
